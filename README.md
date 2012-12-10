2b216d61acb2b55bfde795d1a9f59d951b9c55bd
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2012-12-10 17:54:02 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+This gem protects you against most opportunistic attacks and is at the same time as simple as possible.
+
+Usage:
+
+``` ruby
+require 'almost-rack-protection'
+
+use Almost::Rack::Protection
+run MyApp
+```
---
+++ lib/almost-rack-protection.rb
@@ -0,0 +1,39 @@
+module Almost
+  module Rack
+    class Protection
+      attr_reader :app, :attack_status, :attack_message, :attack_type
+
+      def initalize(app, options = {})
+        @app            = app
+        @attack_status  = options.fetch(:attack_status,   403)
+        @attack_message = options.fetch(:attack_message, 'Attack Prevented')
+        @attack_type    = options.fetch(:attack_type,    'text/plain')
+      end
+
+      def call(env)
+        request = Rack::Request.new(env)
+        reply_for(request.scheme)
+      end
+
+      private
+
+      def attack_headers
+        { 'Content-Type' => attack_type }
+      end
+
+      def prevent!
+        $stderr.puts "Attack Prevented"
+        [attack_status, attack_headers, [attack_message]]
+      end
+
+      def reply_for(scheme)
+        case scheme
+        when 'http'   then prevent!
+        when 'https'  then prevent!
+        when 'coffee' then [418, attack_headers, "I'm a teapot!"]
+        else app.call(env)
+        end
+      end
+    end
+  end
+end
